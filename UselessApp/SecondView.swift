2a5d4a5a76835cfa5681bc5f68239fdfb6379d65
iosdevrashad <rashad8787@me.com>
2023-05-13 15:09:28 -0500

    Created Second View

---
+++ UselessApp/SecondView.swift
@@ -0,0 +1,15 @@
+//
+//  SecondView.swift
+//  UselessApp
+//
+//  Created by Rashad Surratt on 5/13/23.
+//
+
+import Foundation
+
+// A Test for the second view.
+
+
+struct Evgenie {
+    let age: Int
+}
