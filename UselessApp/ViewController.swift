029276dee2344ab0049674f390b87ea8f8bd9aec
iosdevrashad <rashad8787@me.com>
2023-05-13 14:55:05 -0500

    Initial Commit

---
+++ UselessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UselessApp
+//
+//  Created by Rashad Surratt on 5/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
